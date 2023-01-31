6637adb8786132a6c1d457b0514113619ad963c3
Ricard Lado <ricardlado@hotmail.com>
2023-01-31 23:06:26 +0100

    Finished implementing basic mat_math - Wrote some tests for the new functions (examples missing) (not complete) - Changed casing on package name (to avoid warinings)

--- src/mat_math.rs
+++ src/mat_math.rs
@@ -1,11 +1,57 @@
 //! Basic dense matrix and vector operations
 //! 
 
+/// Defines an additive identity element for `Self`.
+///
+/// # Deriving
+///
+/// This trait can be automatically be derived using `#[deriving(Zero)]`
+/// attribute. If you choose to use this, make sure that the laws outlined in
+/// the documentation for `Zero::zero` still hold.
+pub trait Zero: Sized + std::ops::Add<Self, Output = Self> {
+    /// Returns the additive identity element of `Self`, `0`.
+    ///
+    /// # Laws
+    ///
+    /// ```{.text}
+    /// a + 0 = a       ∀ a ∈ Self
+    /// 0 + a = a       ∀ a ∈ Self
+    /// ```
+    /// 
+    fn zero() -> Self;
+
+    /// Returns `true` if `self` is equal to the additive identity.
+    fn is_zero(&self) -> bool;
+}
+
+macro_rules! zero_impl {
+    ($t:ty, $v:expr) => {
+        impl Zero for $t {
+            #[inline]
+            fn zero() -> $t { $v }
+            #[inline]
+            fn is_zero(&self) -> bool { *self == $v }
+        }
+    }
+}
+
+zero_impl!(usize, 0usize);
+zero_impl!(u32,  0u32);
+zero_impl!(u64,  0u64);
+zero_impl!(isize, 0isize);
+zero_impl!(i32, 0i32);
+zero_impl!(i64, 0i64);
+zero_impl!(f32, 0.0f32);
+zero_impl!(f64, 0.0f64);
+
+
 /// Trait defining numbers that can perform mathematical operations
 /// 
-pub trait Num {}
+pub trait Num: Zero+std::ops::Add+std::ops::AddAssign+std::ops::Mul+std::ops::MulAssign+Copy+std::fmt::Debug{}
+impl Num for usize{}
 impl Num for u32 {}
 impl Num for u64 {}
+impl Num for isize {}
 impl Num for i32 {}
 impl Num for i64 {}
 impl Num for f32 {}
@@ -14,34 +60,64 @@ impl Num for f64 {}
 
 /// Scalar plus dense matrix
 /// 
-/// `mat` is overwritten with the answer
-/// 
-pub fn scpmat<T: Num+std::ops::AddAssign+Copy>(scalar: T, mat: &mut Vec<Vec<T>>) {
+pub fn scpmat<T: Num>(scalar: T, mat: &Vec<Vec<T>>) -> Vec<Vec<T>> {
+    let rows = mat.len();
+    let columns = mat[0].len();
+
+    let mut r = vec![vec![T::zero();columns];rows];
     for i in 0..mat.len(){
         for j in 0..mat[0].len(){
-            mat[i][j] += scalar;
+            r[i][j] = mat[i][j] + scalar;
         }
     }
+
+    return r;
 }
 
 /// Scalar times dense matrix
 /// 
-/// `mat` is overwritten with the answer
-/// 
-pub fn scxmat<T: Num+std::ops::MulAssign+Copy>(scalar: T, mat: &mut Vec<Vec<T>>) {
+pub fn scxmat<T: Num + std::ops::Mul<Output = T>>(scalar: T, mat: &Vec<Vec<T>>) -> Vec<Vec<T>> {
+    let rows = mat.len();
+    let columns = mat[0].len();
+
+    let mut r = vec![vec![T::zero();columns];rows];
     for i in 0..mat.len(){
         for j in 0..mat[0].len(){
-            mat[i][j] *= scalar;
+            r[i][j] = mat[i][j] * scalar;
         }
     }
+
+    return r;
+}
+
+/// Scalar plus dense vector
+/// 
+pub fn scpvec<T: Num>(scalar: T, v: &Vec<T>) -> Vec<T>{
+    let mut r = vec![T::zero(); v.len()];
+    for i in 0..v.len(){
+        r[i] = v[i] + scalar;
+    }
+
+    return r;
+}
+
+/// Scalar times dense vector
+/// 
+pub fn scxvec<T: Num + std::ops::Mul<Output = T>>(scalar: T, v: &Vec<T>) -> Vec<T> {
+    let mut r = vec![T::zero(); v.len()];
+    for i in 0..v.len(){
+        r[i] = v[i] * scalar;
+    }
+
+    return r;
 }
 
 /// Dense vector addition
 ///
-pub fn add_vec<T: Num+std::ops::Add<Output = T>+Copy>(a: &Vec<T>, b: &Vec<T>) -> Vec<T> {
+pub fn add_vec<T: Num+std::ops::Add<Output = T>>(a: &Vec<T>, b: &Vec<T>) -> Vec<T> {
     let len = a.len();
     if len != b.len() {
-        panic!("Vector sizes do not match.");
+        panic!("Vector dimensions do not match.");
     }
 
     let mut c = Vec::with_capacity(len);
@@ -51,17 +127,14 @@ pub fn add_vec<T: Num+std::ops::Add<Output = T>+Copy>(a: &Vec<T>, b: &Vec<T>) ->
     return c;
 }
 
-// TODO: Dense vector cross and dot product
-
-
-//// Matrix add
+//// Dense matrix add
 ///
-pub fn add_mat <T: Num+std::ops::Add<Output = T>+Copy>(a: &Vec<Vec<T>>, b: &Vec<Vec<T>>) -> Vec<Vec<T>> {
+pub fn add_mat <T: Num+std::ops::Add<Output = T>>(a: &Vec<Vec<T>>, b: &Vec<Vec<T>>) -> Vec<Vec<T>> {
     // Check sizes
     let len_i = a.len();
     let len_j = a[0].len();
     if len_i != b.len() || len_j != b[0].len() {
-        panic!("Matrices sizes do not match.");
+        panic!("Matrix dimensions do not match.");
     }
 
     // Add
@@ -78,20 +151,42 @@ pub fn add_mat <T: Num+std::ops::Add<Output = T>+Copy>(a: &Vec<Vec<T>>, b: &Vec<
 }
 
 
-//// Matrix multiplication
-/*
-    for (int i = 0; i < R1; i++) {
-        for (int j = 0; j < C2; j++) {
-            rslt[i][j] = 0;
- 
-            for (int k = 0; k < R2; k++) {
-                rslt[i][j] += mat1[i][k] * mat2[k][j];
-            }
- 
-            printf("%d\t", rslt[i][j]);
-        }
- 
-        printf("\n");
+//// Dense matrix multiplication
+/// 
+pub fn mul_mat<T: Num + std::ops::AddAssign<<T as std::ops::Mul>::Output>> (a: &Vec<Vec<T>>, b: &Vec<Vec<T>>) -> Vec<Vec<T>> {
+    let rows_a = a.len();
+    let columns_a = a[0].len();
+    let rows_b = b.len();
+    let columns_b = b[0].len();
+
+    if columns_a != rows_b{
+        panic!("Matrix dimensions do not match.")
     }
 
-*/
+    let mut r = vec![vec![T::zero();columns_b];rows_a];
+    for i in 0..rows_a{
+        for j in 0..columns_b{
+            for k in 0..rows_b{
+                r[i][j] += a[i][k]*b[k][j];
+            }
+        }
+    }
+
+    return r;
+} 
+
+/// Transpose a dense matrix
+/// 
+pub fn transpose<T: Num> (a: &Vec<Vec<T>>) -> Vec<Vec<T>>{
+    let rows = a.len();
+    let columns = a[0].len();
+
+    let mut r = vec![vec![T::zero();rows];columns];
+    for i in 0..rows{
+        for j in 0..columns{
+            r[j][i] = a[i][j];
+        }
+    }
+
+    return r;
+}
