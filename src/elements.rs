37215f97ed61a9753c5216b9f79497382effaf73
Ricard Lado <ricardlado@hotmail.com>
2023-02-01 23:13:06 +0100

    Mid implementation of the stiffness matrix

--- src/elements.rs
+++ src/elements.rs
@@ -3,7 +3,7 @@
 //! 
 //! Elements are generally named after the number of DoF they have. 
 
-use crate::utils::Coord3D;
+use crate::{utils::Coord3D, mat_math::scxmat};
 
 /// Calculate the distance between two `f64` points in 3D space [a -> b]
 /// 
@@ -28,17 +28,19 @@ pub fn dist_3df(a: &Coord3D<f64>, b: &Coord3D<f64>) -> f64 {
 #[derive(Clone, Debug)]
 pub struct Beam12{
     /// Nodes
-    nodes: [Coord3D<f64>;2], 
+    pub nodes: [Coord3D<f64>;2], 
     /// Elastic modulus
-    e: f64,
+    pub e: f64,
     /// Shear modulus
-    g: f64,
-    /// Moment of inertia {Ix,Iy,Iz}
-    i: Coord3D<f64>,
+    pub g: f64,
+    /// Moment of inertia {None,Iy,Iz}
+    pub i: Coord3D<f64>,
+    /// Torsional constant of cross-section [m^4]
+    pub j: f64,
     /// Area {Ax,Ay,Az}
-    a: Coord3D<f64>,
+    pub a: Coord3D<f64>,
     /// Stiffness matrix
-    stff: Vec<Vec<f64>>,
+    pub stff: Vec<Vec<f64>>,
 }
 
 impl Beam12{
@@ -51,15 +53,16 @@ impl Beam12{
     /// - i: Moment of inertia {Ix,Iy,Iz}
     /// - a: Area {Ax,Ay,Az}
     /// 
-    pub fn new(nodes: [Coord3D<f64>;2], e: f64, g: f64, i: Coord3D<f64>, a: Coord3D<f64>) -> Beam12 {
+    pub fn new(nodes: [Coord3D<f64>;2], e: f64, g: f64, i: Coord3D<f64>, j: f64, a: Coord3D<f64>) -> Beam12 {
         // create an instance of Beam12
         let mut elem = Beam12{
             nodes: nodes,
             e: e,
             g: g,
             i: i,
+            j: j,
             a: a,
-            stff: vec![vec![0.;12];12],
+            stff: Vec::new(),
         };
 
         // calculate elements properties
@@ -69,7 +72,42 @@ impl Beam12{
         let phi_y = 12.*(elem.e*elem.i.y/(elem.g*elem.a.y*elem_len.powi(2)));
         let phi_z = 12.*(elem.e*elem.i.z/(elem.g*elem.a.z*elem_len.powi(2)));
 
-        
+        let kz = scxmat(
+            elem.e*elem.i.z/((1. + phi_y)*elem_len.powi(2)), 
+            &vec![
+                vec![12., 6.*elem_len, -12., 6.*elem_len],
+                vec![0., (4.+phi_y)*elem_len.powi(2), -6.*elem_len, (2.-phi_y)*elem_len.powi(2)],
+                vec![0., 0., 12., -6.*elem_len],
+                vec![0., 0., 0., (4.+phi_y)*elem_len.powi(2)]
+            ]
+        );
+        let ky = scxmat(
+            elem.e*elem.i.y/((1. + phi_z)*elem_len.powi(2)), 
+            &vec![
+                vec![12., -6.*elem_len, -12., -6.*elem_len],
+                vec![0., (4.+phi_z)*elem_len.powi(2), 6.*elem_len, (2.-phi_z)*elem_len.powi(2)],
+                vec![0., 0., 12., 6.*elem_len],
+                vec![0., 0., 0., (4.+phi_z)*elem_len.powi(2)]
+            ]
+        );
+
+        let eal = elem.e*elem.a.x/elem_len;
+        let gkl = elem.g*elem.j/elem_len;
+        elem.stff = vec![
+            vec![eal, 0., 0., 0., 0., 0., -eal, 0., 0., 0., 0., 0.], // 1
+            vec![0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.], // 2
+            vec![0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.], // 3
+            vec![0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.], // 4
+            vec![0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.], // 5
+            vec![0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.], // 6
+            vec![0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.], // 7
+            vec![0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.], // 8
+            vec![0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.], // 9
+            vec![0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.], // 10
+            vec![0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.], // 11
+            vec![0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.], // 12
+        ];
+
 
         // return the element
         return elem;
